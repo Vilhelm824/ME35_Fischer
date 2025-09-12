24f2835ec201fbebb6c2ed89c9eeb4010bc761f1
Vilhelm824 <willhfischer@icloud.com>
2025-09-12 12:23:03 -0400

    Day3 added, unused code deleted

--- Day_2_finding_mac.py
+++
@@ -1,8 +0,0 @@
-import network
-wlan = network.WLAN()
-mac = wlan.config("mac")
-print(mac)
-
-import ubinascii
-mac_readable = ubinascii.hexlify(mac,":").decode()
-print(mac_readable)
--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
-SSID="45-Bowdoin" # "tufts_eecs"
-PWD="4Bowdoin5x" # "foundedin1883"
+SSID="tufts_eecs" # "45-Bowdoin" 
+PWD="foundedin1883" # "4Bowdoin5x"
 at_api_token ="patcxZbmZNg1bN4Vs.a778f0145d065ec3a566618df815be86798471426e21d75f06d67e247f84cf58"
