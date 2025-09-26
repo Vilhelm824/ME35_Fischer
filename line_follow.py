b335dd00739921014c44f5120672375bd4f51559
Vilhelm824 <willhfischer@icloud.com>
2025-09-26 13:17:50 -0400

    finished line follower

--- line_follow.py
+++ line_follow.py
@@ -1,14 +1,15 @@
-import time
+import time, neopixel
 from machine import SoftI2C, Pin
 from veml6040 import VEML6040
 from encoder import Motor
 
 
-def readColor(sensor):
+def readColor(sensorR, sensorL):
     try:
-        sensor.trigger_measurement()
-        red, green, blue, white = sensor.read_rgbw()
-        return red, green, blue, white
+        sensorR.trigger_measurement()
+        sensorL.trigger_measurement()
+        data = (sensorR.read_rgbw(), sensorL.read_rgbw())
+        return data
     except OSError as e:
         if e.args[0] == 19: # errno 19 is ENODEV (No such device)
             print("I2C Error: VEML6040 not responding. Check wiring.")
@@ -16,42 +17,65 @@ def readColor(sensor):
             print(f"I2C Read Error: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+    
 
-# sensor value threshold for white vs black
-threshold = 15000
 
-# initialize motor
-motor = Motor(14,27, 32,39)
+# initialize motors
+motorR = Motor(13, 12, 32,39)
+motorL = Motor(27, 14, 32,39)
+# initialize neopixel
+np = neopixel.NeoPixel(Pin(15), 2)
+
 
 # initialize I2C
+# two channels bc 2 sensors w/ same addr
 try:
-    i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000) #Check for I2C pins from pinouts
+    i2cR = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000) #Check for I2C pins from pinouts
+    i2cL = SoftI2C(scl=Pin(32), sda=Pin(33), freq=100000) #Check for I2C pins from pinouts
     print("I2C initialized successfully.")
 except Exception as e:
     print(f"Error initializing I2C: {e}")
     exit()
 
-# initialize color sensor
+# initialize color sensors
 try:
-    sensor = VEML6040(i2c)
+    sensorR = VEML6040(i2cR)
+    sensorL = VEML6040(i2cL)
     print("VEML6040 sensor object created.")
 except Exception as e:
     print(f"Error creating VEML6040 object: {e}")
     exit()
 
 
-red, green, blue, white = readColor(sensor)
-print(f"Red: {red}, Green: {green}, Blue: {blue}, White: {white}")
+# sensor value threshold for white vs black
+thresh = 550
+motorSpeed = 30
+
+# change sensor integration time to 40ms
+IT_40MS = (0b000 << 4) # 40 ms
+sensorR.set_integration_time(IT_40MS)
+sensorL.set_integration_time(IT_40MS)
 
 
-while True:
+while(True):
     # get color data
-    red, green, blue, white = readColor(sensor)
-    print(white)
-    if(white < threshold):
-        # slow down
-        motor.stop()
-    else:
-        motor.setSpeed(0, 20)
-    time.sleep(0.5)
-        
+    colorData = readColor(sensorR, sensorL)
+    whiteR = colorData[0][3]
+    whiteL = colorData[1][3]
+    print(colorData)
+    if(whiteR > thresh and whiteL > thresh):
+        # go forward
+        motorR.setSpeed(0, motorSpeed)
+        motorL.setSpeed(0, motorSpeed)
+    elif(whiteR < thresh and whiteL > thresh):
+        # turn right
+        motorR.setSpeed(1, motorSpeed)
+        motorL.setSpeed(0, motorSpeed)
+    elif(whiteR > thresh and whiteL < thresh):
+        # turn left
+        motorR.setSpeed(0, motorSpeed)
+        motorL.setSpeed(1, motorSpeed)
+    elif(whiteR < thresh and whiteL < thresh):
+        # stop
+        motorR.stop()
+        motorL.stop()
